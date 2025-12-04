b61cde87fd8e6ce0343e860d0ac59c8600ef2d6b
Niyomi Rabdu <Niyomi_Rabdu@musikaar.com>
2025-12-04 11:36:08 +0530

    Import data from CSV file

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,23 +2,11 @@ from  playwright.sync_api import Page
 class loginPage:
     def __init__(self,page:Page):
         self.page = page
-        self.userName = page.locator("[data-test=\"username\"]")
-        self.password = page.locator("[data-test=\"password\"]")
-        self.loginButton = page.locator("[data-test=\"login-button\"]")
-   
-   #atomic methods, functions per single action
-    
-    def enter_userName(self,username:str):
-        self.userName.fill("standard_user")
-        
-    def enter_password(self, password:str):
-        self.password.fill("secret_sauce")       
-        
-    def click_login(self):
-        self.loginButton.click()
-        
-   #single method, functions per functionality
+        self.userName = page.get_by_role("textbox", name="Username")
+        self.password = page.get_by_role("textbox", name="Password")
+        self.loginButton = page.get_by_role("button", name="Login")
+      
     def login(self, username:str, password:str):
-        self.userName.fill("standard_user")
-        self.password.fill("secret_sauce")
+        self.userName.fill(username)
+        self.password.fill(password)
         self.loginButton.click()
--- tests/UI/test_loginPage.py
+++ tests/UI/test_loginPage.py
@@ -1,7 +1,19 @@
+import pytest
 from pages.login_page import loginPage
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
-def test_example(page: Page) -> None:
+def get_csv_data() -> list:
+    import csv
+    data = []
+    with open('./test_data/login_data.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data
+
+@pytest.mark.parametrize("username,password", get_csv_data())
+def test_login(page: Page,username,password) -> None:
     login_page = loginPage(page)
-    page.goto("https://www.saucedemo.com/")
-    login_page.login("standard_user","secret_sauce")
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    login_page.login(username,password)
+    expect(page.get_by_role("link", name="Dashboard")).to_be_visible()
